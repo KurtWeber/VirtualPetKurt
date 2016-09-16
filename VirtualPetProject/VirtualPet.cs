ae1e04b67dfec27c2cde4e746825f94cce58f3fe
KurtWeber <weberkrt@msn.com>
2016-09-16 16:43:54 -0400

    Created Class map

---
+++ VirtualPetProject/VirtualPet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPetProject
+{
+    class VirtualPet
+    {
+        // Fields
+        
+        // Properties
+        // -Hungry
+        // -Waste
+        // -Boredom
+
+        // Constructors
+        
+        // Methods
+        // -Feed
+        // -Let out
+        // -Play 
+        // -To vet
+        // -Tick
+    }
+}
